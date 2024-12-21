181130eb98202ae4c487cede43e51c557566f814
Vasanthan thamilmani <vasanthan4u2005@gmail.com>
2024-12-21 09:13:35 +0530

    Message

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const dbconnect=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root123",
-    database:"empmanage"
+const dbconnect = mysql.createConnection({
+  host: process.env.DB_host || "localhost",
+  user: process.env.DB_user || "root",
+  password: process.env.DB_password || "root123",
+  database: process.env.DB_database || "empmanage",
 });
 
-dbconnect.connect((err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log("DB Connection succesfull");
-    }
+dbconnect.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("DB Connection succesfull");
+  }
 });
 
-module.exports=dbconnect;
+module.exports = dbconnect;
